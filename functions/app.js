a60376c685a7efc6c804a62156f161fbd1df038c
Daniel Gorbachev <dg186533@gmail.com>
2024-12-11 13:19:12 -0500

    added dependencies for netlify deployment

--- functions/app.js
+++ functions/app.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const path = require('path');
 const app = express()
+const router = express.Router();
 const port = 3000
 const indexRoute = require("../routes");
 const formRoute = require("../routes");
 const formSubmission = require("../routes");
 
+
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 //console.log('Current working directory:', process.cwd());
-console.log('Static files are served from:', path.join(__dirname, '../public'));
-
 
 app.use("/", indexRoute);
 app.use("/new", formRoute);
 app.use("/new", formSubmission);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use('/.netlify/functions/submitMessage', router)
+
+module.exports = app;
+
+module.exports.handler = serverless(app)
